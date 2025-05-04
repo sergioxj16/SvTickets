c179f1dc57aa88844457ede2ff00edcd3ffa9779
jingyi <jingyixj@gmail.com>
2025-05-04 19:04:11 +0200

    polyfills.ts deleted

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideExperimentalZonelessChangeDetection(),
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
